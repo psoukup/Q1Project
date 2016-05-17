ee502459800adf966502a982a78408b852d0e423
p <p@ps-MacBook-Air.local>
2016-05-17 16:36:03 -0500

    changed

--- assets/trial.js
+++ assets/trial.js
@@ -1,4 +1,8 @@
 $(function() {
+  var year = 2016;
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  $("th").append("<p><span><button type='button' class='btn glyphicon glyphicon-chevron-left'></button>" + " " + months[6q] + " " + year + " <span><button type='button' class='btn glyphicon glyphicon-chevron-right'></button></p>")
   $('td').on("click", function() {
     $(this).append("<div class='ribbon'></div>")
     $('ribbon').addClass("ribbon")
