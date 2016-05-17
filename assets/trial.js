fcc96c2dd2f401dbe024be5cd6d53ffd7a735a42
p <p@ps-MacBook-Air.local>
2016-05-17 16:19:38 -0500

    furthering table approach

--- assets/trial.js
+++ assets/trial.js
@@ -1,3 +1,9 @@
 $(function() {
-  
+  $('td').on("click", function() {
+    $(this).append("<div class='ribbon'></div>")
+    $('ribbon').addClass("ribbon")
+  })
+  $('.ribbon').on("click", function() {
+    $(this).append("<form type='text' class='todo'></form>")
+    })
 })
