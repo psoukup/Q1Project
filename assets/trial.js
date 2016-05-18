d43d1055257dbec25ac7ef2aeaaf220bfa71b20e
p <p@ps-MacBook-Air.local>
2016-05-18 10:52:49 -0500

    fixed top year and month

--- assets/trial.js
+++ assets/trial.js
@@ -1,13 +1,101 @@
 $(function() {
   var year = 2016;
   var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+  var jan2016 = new Date(2016, 0, 01)
+  var p = jan2016.getMonth();
+  var weekday = jan2016.getDay();
+  console.log(p)
 
-  $("th").append("<p><span><button type='button' class='btn glyphicon glyphicon-chevron-left'></button>" + " " + months[6q] + " " + year + " <span><button type='button' class='btn glyphicon glyphicon-chevron-right'></button></p>")
+
+  $("th").append("<span><button type='button' id='left' class='btn glyphicon glyphicon-chevron-left'></button>" + " " + "<p class='month'> " + months[p] + " " + year + "</p>" + " <span><button type='button' id='right' class='btn glyphicon glyphicon-chevron-right'></button>")
+
   $('td').on("click", function() {
-    $(this).append("<div class='ribbon'></div>")
-    $('ribbon').addClass("ribbon")
-  })
+    $(this).append("<div class='ribbon'></div>");
+    $('ribbon').addClass("ribbon");
+  });
   $('.ribbon').on("click", function() {
-    $(this).append("<form type='text' class='todo'></form>")
+      console.log('clicked');
+    $(this).append("<form type='text' class='todo'></form>");
+  });
+
+    $('#right').on("click", function() {
+      if (p < 11) {
+        p += 1;
+      } else {
+        p = 0;
+        year += 1;
+      }
+      console.log(p);
+      console.log(year);
+      $(".month").text(months[p] + " " + year);
+    });
+    $('#left').on("click", function() {
+      if (p > 0 && p < 12) {
+        p -= 1;
+      } else {
+        p = 12;
+        p -= 1;
+        year -= 1;
+      }
+      console.log(p)
+      console.log(year)
+      $(".month").text(months[p] + " " + year);
     })
+
+    var datestring = $(".month").text();
+    console.log(datestring)
+    console.log( typeof datestring )
+    var datedata = Date.parse(datestring)
+    console.log(datedata)
+    var date = new Date(datedata);
+    console.log(date)
+    var dayz = date.getDay();
+    console.log(dayz);
+    var daydate = ((date.toJSON()).slice(5, 7));
+    console.log(daydate);
+
+    if (dayz === 0) {
+      console.log("Sunday")
+      $(".sunday").append("<p>" + daydate + "</p>")
+    }
+    else if (dayz === 1) {
+      console.log("Monday");
+      $(".monday").append("<p>" + daydate + "</p>")
+    }
+    else if (dayz === 2) {
+      console.log("Tuesday");
+      $(".tuesday").append("<p>" + daydate + "</p>")
+    }
+    else if (dayz === 3) {
+      console.log("Wednesday");
+      $(".wednesday").append("<p>" + daydate + "</p>")
+    }
+    else if (dayz === 4) {
+      console.log("Thursday");
+      $(".thursday").append("<p>" + daydate + "</p>")
+    }
+    else if (dayz === 5) {
+      console.log("Friday");
+      $(".friday").append("<p>" + daydate + "</p>")
+    }
+    else if (dayz === 6) {
+      console.log("Saturday");
+      $(".saturday").append("<p>" + daydate + "</p>")
+    }
+    // $('#right').on("click", function() {
+    //   if (weekday < 6) {
+    //     weekday += 1;
+    //   } else {
+    //     weekday = 0;
+    //   }
+    // })
+    // $('#left').on("click", function() {
+    //   if (weekday >= 0 && weekday < 6) {
+    //     p -= 1;
+    //   } else {
+    //     weekday = 6;
+    //     p -= 1;
+    //   }
+    //   console.log(weekday)
+    // })
 })
