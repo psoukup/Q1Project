03acd25f7da740fec8892011d14d07cd4c3bbbbe
p <p@ps-MacBook-Air.local>
2016-05-19 10:20:00 -0500

    cleaned up code and added comments

--- assets/trial.js
+++ assets/trial.js
@@ -1,26 +1,43 @@
 $(function() {
     var year = 2016;
     var daysinmonth = [];
+
+//Setting the leapYear
     function leapYear(year) {
-      if (year % 4 === 0 && year % 100 != 0 || year % 400 === 0) {
-        daysinmonth[2] === 29;
-      } else {
-        daysinmonth[2] === 28;
-      }
+        if (year % 4 === 0 && year % 100 != 0 || year % 400 === 0) {
+            daysinmonth[2] === 29;
+        } else {
+            daysinmonth[2] === 28;
+        }
     }
     leapYear(year);
 
+//array data for number of days per month, months per year, and days of the week
     var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     var daysinmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    // var arrayDays = ['mondy']
-    // $('.'+days[day])
+    var arrayDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+//formula to
+    $('.monday').append('<p>' + arrayDays[0] + '</p>');
+    $('.tuesday').append('<p>' + arrayDays[1] + '</p>');
+    $('.wednesday').append('<p>' + arrayDays[2] + '</p>');
+    $('.thursday').append('<p>' + arrayDays[3] + '</p>');
+    $('.friday').append('<p>' + arrayDays[4] + '</p>');
+    $('.saturday').append('<p>' + arrayDays[5] + '</p>');
+    console.log(arrayDays[0])
+
+//Calling the leapYear function
+
     leapYear(year);
+
+//Initializing calendar app date
     var jan2016 = new Date(2016, 01, 01)
     var p = jan2016.getMonth();
     var weekday = jan2016.getDay();
     console.log(p)
 
-
+//Buttons that append the year and month when clicked left to right
     $("th").append("<span><button type='button' id='left' class='btn glyphicon glyphicon-chevron-left'></button>" + " " + "<p class='month'> " + months[p] + " " + year + "</p>" + " <span><button type='button' id='right' class='btn glyphicon glyphicon-chevron-right'></button>")
 
     $('td').on("click", function() {
@@ -33,6 +50,7 @@ $(function() {
         $(this).append("<form type='text' class='todo'></form>");
     });
 
+//Formulas to get the proper month data
     $('#right').on("click", function() {
         if (p < 11) {
             p += 1;
@@ -45,90 +63,47 @@ $(function() {
         $(".month").text(months[p] + " " + year);
     });
     $('#left').on("click", function() {
-        if (p > 0 && p < 12) {
-            p -= 1;
-        } else {
-            p = 12;
-            p -= 1;
-            year -= 1;
-        }
-        console.log(p)
-        console.log(year)
-        $(".month").text(months[p] + " " + year);
-    })
+            if (p > 0 && p < 12) {
+                p -= 1;
+            } else {
+                p = 12;
+                p -= 1;
+                year -= 1;
+            }
+            console.log(p)
+            console.log(year)
+            $(".month").text(months[p] + " " + year);
+        })
 
 
-    for ( var i = 1; i <= daysinmonth[p]; i++) {
-    $('td').append("<p>" + i + "</p>")
+//Formula to add (numbers) days to each box in the table
+    for (var i = 1; i <= daysinmonth[p]; i++) {
+        $('td').append("<p>" + i + "</p>")
     }
 
 
-    var datestring = $(".month").text();
-    console.log(datestring)
-    console.log(typeof datestring)
-    var datedata = Date.parse(datestring)
-    console.log(datedata)
-    var date = new Date(datedata);
-    console.log(date)
-    var dayz = date.getDay();
-    console.log(dayz);
-    var daydate = ((date.toJSON()).slice(5, 7));
-    console.log(daydate);
-
-    if (dayz === 0) {
-        console.log("Sunday")
-        $(".sunday").append("<p>" + daydate + "</p>")
-    } else if (dayz === 1) {
-        console.log("Monday");
-        $(".monday").append("<p>" + daydate + "</p>")
-    } else if (dayz === 2) {
-        console.log("Tuesday");
-        $(".tuesday").append("<p>" + daydate + "</p>")
-    } else if (dayz === 3) {
-        console.log("Wednesday");
-        $(".wednesday").append("<p>" + daydate + "</p>")
-    } else if (dayz === 4) {
-        console.log("Thursday");
-        $(".thursday").append("<p>" + daydate + "</p>")
-    } else if (dayz === 5) {
-        console.log("Friday");
-        $(".friday").append("<p>" + daydate + "</p>")
-    } else if (dayz === 6) {
-        console.log("Saturday");
-        $(".saturday").append("<p>" + daydate + "</p>")
-    }
-    // $('#right').on("click", function() {
-    //   if (weekday < 6) {
-    //     weekday += 1;
-    //   } else {
-    //     weekday = 0;
-    //   }
-    // })
-    // $('#left').on("click", function() {
-    //   if (weekday >= 0 && weekday < 6) {
-    //     p -= 1;
-    //   } else {
-    //     weekday = 6;
-    //     p -= 1;
-    //   }
-    //   console.log(weekday)
-    // })
+//test to see if zipcode textbox was working - it isnt yet
     $('#zipcode').on("click", function() {
-      console.log("clikzzzz");
-      console.log($('#zipcode').val())
+        console.log("clikzzzz");
+        console.log($('#zipcode').val())
     });
+
+    //AJAX call for weather from text form asking zip code input
+
     var zipcode = ($('#zipcode').val());
+
     // zipcode = 88888;
     // console.log(zipcode)
+    
     $.ajax({
-      url: "http://api.openweathermap.org/data/2.5/weather?zip=" + ($('#zipcode').val()) + ",us&APPID=b3df5655b9b91983bf112d6eef2da821",
-      method: "get",
-      success: function(data) {
-        var skys = data.weather[0].main
-        var kelvin = data.main.temp;
-        var farenheight = Math.round(1.8 * (kelvin - 273) + 32)
-        console.log("Current temperature: " + farenheight + ", " + "current conditions: " + skys)
-        $('th').append("<p class='weathersmall'>Current temperature: " + farenheight + ", " + "Current conditions: " + skys + "</p>")
-      }
+        url: "http://api.openweathermap.org/data/2.5/weather?zip=" + ($('#zipcode').val()) + ",us&APPID=b3df5655b9b91983bf112d6eef2da821",
+        method: "get",
+        success: function(data) {
+            var skys = data.weather[0].main
+            var kelvin = data.main.temp;
+            var farenheight = Math.round(1.8 * (kelvin - 273) + 32)
+            console.log("Current temperature: " + farenheight + ", " + "current conditions: " + skys)
+            $('th').append("<p class='weathersmall'>Current temperature: " + farenheight + ", " + "Current conditions: " + skys + "</p>")
+        }
     })
 })
